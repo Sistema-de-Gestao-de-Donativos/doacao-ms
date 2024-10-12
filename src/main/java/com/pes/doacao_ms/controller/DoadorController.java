c9f0a6fcf49b081a625c76b3e95280bb1e0f9124
cassiano-flores <cassiano.flores2424@gmail.com>
2024-10-12 15:15:44 -0300

    update swagger configs

--- src/main/java/com/pes/doacao_ms/controller/DoadorController.java
+++ src/main/java/com/pes/doacao_ms/controller/DoadorController.java
@@ -5,12 +5,14 @@ import com.pes.doacao_ms.controller.request.IncludeDoadorRequest;
 import com.pes.doacao_ms.controller.response.DoadorIdResponse;
 import com.pes.doacao_ms.controller.response.DoadorResponse;
 import com.pes.doacao_ms.service.DoadorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/v1/doador")
@@ -20,14 +22,23 @@ public class DoadorController {
     @Autowired
     private DoadorService doadorService;
 
+    @Operation(summary = "Get a Doador.", description = "Return a specific Doador by Email.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Doador found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = DoadorResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Invalid request body\"}"))),
+            @ApiResponse(responseCode = "404", description = "Doador not found", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Doador not found\"}"))),
+    })
     @GetMapping
-    @ResponseStatus(OK)
     public DoadorResponse get(@Valid @RequestBody EmailRequest emailRequest) {
         return doadorService.getDoador(emailRequest);
     }
 
+    @Operation(summary = "Create a new Doador.", description = "Create a new Doador using the given request.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Doador created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = DoadorIdResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": [\"Name is required\",\n" + "\"Document is required\"]}")))
+    })
     @PostMapping
-    @ResponseStatus(CREATED)
     public DoadorIdResponse include(@Valid @RequestBody IncludeDoadorRequest includeDoadorRequest) {
         return doadorService.include(includeDoadorRequest);
     }
--- src/main/java/com/pes/doacao_ms/controller/ItemController.java
+++ src/main/java/com/pes/doacao_ms/controller/ItemController.java
@@ -5,12 +5,14 @@ import com.pes.doacao_ms.controller.request.ItemIdRequest;
 import com.pes.doacao_ms.controller.response.ItemIdResponse;
 import com.pes.doacao_ms.controller.response.ItemResponse;
 import com.pes.doacao_ms.service.ItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/v1/item")
@@ -20,14 +22,23 @@ public class ItemController {
     @Autowired
     private ItemService itemService;
 
+    @Operation(summary = "Get an Item.", description = "Return a specific Item by ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Item found", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ItemResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Invalid request body\"}"))),
+            @ApiResponse(responseCode = "404", description = "Item not found", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": \"Item not found\"}"))),
+    })
     @GetMapping
-    @ResponseStatus(OK)
     public ItemResponse get(@Valid @RequestBody ItemIdRequest itemIdRequest) {
         return itemService.getItem(itemIdRequest);
     }
 
+    @Operation(summary = "Create a new Item.", description = "Create a new Item using the given request.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Item created", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ItemIdResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request body", content = @Content(mediaType = "application/json", schema = @Schema(example = "{\"error\": [\"Name is required\"]}")))
+    })
     @PostMapping
-    @ResponseStatus(CREATED)
     public ItemIdResponse include(@Valid @RequestBody IncludeItemRequest includeItemRequest) {
         return itemService.include(includeItemRequest);
     }
