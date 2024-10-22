ffdead8d4ccffb438d64055a7149bd6c047b048c
aSchemitt <alison.schemitt@edu.pucrs.br>
2024-10-22 08:56:57 -0300

    fixed dependencies in pom & commented out some feign client classes unused

--- src/main/java/com/pes/doacao_ms/service/EstoqueService.java
+++ src/main/java/com/pes/doacao_ms/service/EstoqueService.java
@@ -1,23 +1,23 @@
-package com.pes.doacao_ms.service;
+// package com.pes.doacao_ms.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Service;
 
-import com.pes.doacao_ms.domain.EstoqueItem;
-import com.pes.doacao_ms.feignClients.estoque.EstoqueRepository;
+// import com.pes.doacao_ms.domain.EstoqueItem;
+// import com.pes.doacao_ms.feignClients.estoque.EstoqueRepository;
 
-@Service
-public class EstoqueService {
+// // @Service
+// public class EstoqueService {
     
-    @Autowired
-    private EstoqueRepository estoqueRepository;
+//     @Autowired
+//     private EstoqueRepository estoqueRepository;
 
-    public void cadastrarItem(){
-        EstoqueItem estoqueItem = new EstoqueItem(null, null, null, null);
-        estoqueRepository.cadastrarItem(estoqueItem);
-    }
+//     public void cadastrarItem(){
+//         EstoqueItem estoqueItem = new EstoqueItem(null, null, null, null);
+//         estoqueRepository.cadastrarItem(estoqueItem);
+//     }
 
-    public EstoqueItem buscarItemEstoque(final String codigoItem){
-        return estoqueRepository.buscarItemEstoque(codigoItem);
-    }
-}
+//     public EstoqueItem buscarItemEstoque(final String codigoItem){
+//         return estoqueRepository.buscarItemEstoque(codigoItem);
+//     }
+// }
