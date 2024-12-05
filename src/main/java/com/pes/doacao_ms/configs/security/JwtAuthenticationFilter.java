aa8f9b5640698cf88969200e83b6c994b8fe2731
aSchemitt <alison.schemitt@edu.pucrs.br>
2024-12-05 15:42:46 -0300

    added specific role check

--- src/main/java/com/pes/doacao_ms/configs/security/JwtAuthenticationFilter.java
+++ src/main/java/com/pes/doacao_ms/configs/security/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.pes.doacao_ms.configs.security;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import java.io.IOException;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,7 +23,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtTokenValidator jwtTokenValidator;
     private final JwtUtil jwtUtil;
-    private final Set<String> validRoles = Set.of("superadmin", "adminCD", "voluntario");
+
+    private final Pattern doadorPattern = Pattern.compile("/v1/doador(/.*)?");
+    private final Pattern doacaoPattern = Pattern.compile("/v1/doacao(/.*)?");
+    private final Set<String> validRoles = Set.of("superadmin","adminAbrigo", "adminCD", "voluntario");
+    private final Set<String> validRolesGetDoador = Set.of("superadmin", "adminCD", "voluntario");
+    private final Set<String> validRolesPostDoador = Set.of("superadmin", "voluntario");
+    private final Set<String> validRolesGetDoacao = Set.of("superadmin", "adminCD", "voluntario");
+    private final Set<String> validRolesPostDoacao = Set.of("superadmin", "voluntario");
 
     @Autowired
     public JwtAuthenticationFilter(JwtTokenValidator jwtTokenValidator, JwtUtil jwtUtil) {
@@ -52,6 +60,31 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 } else {
                     System.out.println("role invalido");
                     throw new ResponseStatusException(FORBIDDEN);
+                }
+                
+                // Restrição de acesso baseada no método HTTP e endpoint 
+                String requestURI = request.getRequestURI(); String method = request.getMethod();
+                
+                if (doadorPattern.matcher(requestURI).matches() && (method.equals("GET"))) {
+                    if (!validRolesGetDoador.contains(role)) {
+                        throw new ResponseStatusException(FORBIDDEN);
+                    }
+                }
+                if (doadorPattern.matcher(requestURI).matches() && (method.equals("POST"))) {
+                    if (!validRolesPostDoador.contains(role)) {
+                        throw new ResponseStatusException(FORBIDDEN);
+                    }
+                }
+
+                if (doacaoPattern.matcher(requestURI).matches() && (method.equals("GET"))) {
+                    if (!validRolesGetDoacao.contains(role)) {
+                        throw new ResponseStatusException(FORBIDDEN);
+                    }
+                }
+                if (doacaoPattern.matcher(requestURI).matches() && (method.equals("POST"))) {
+                    if (!validRolesPostDoacao.contains(role)) {
+                        throw new ResponseStatusException(FORBIDDEN);
+                    }
                 }
 
                 // Configura o JwtUtil com os detalhes
